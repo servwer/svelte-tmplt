30bd2495a4eb1f92f8af38e5f1107b6d032623de
Andrew Astafev <intenopacho@gmail.com>
2022-02-06 14:08:31 +0300

    new feat

--- src/pages/editor.svelte
+++ src/pages/editor.svelte
@@ -1,12 +1,10 @@
 <script>
 let displayedText;
+let selectedText = ''
 const onSave = () => {
+    document.getElementById('test') && document.getElementById('test').remove()
     const wrapper = document.getElementById('wrapper')
-    let newWrap;
-    if (document.getElementById('test')) {
-        document.getElementById('test').remove()
-        document.createElement('div')
-    } else newWrap = document.createElement('div');
+    let newWrap = document.createElement('div');
     newWrap.setAttribute('id', 'test')
     const newArray = displayedText.split('\n')
     newArray.map(item => {
@@ -24,16 +22,54 @@ const onSave = () => {
     })
     wrapper.appendChild(newWrap);
 }
+
+document.addEventListener('keydown', function(event) {
+  if (event.code == 'KeyS' && event.ctrlKey) {
+    event.preventDefault();
+    onSave();
+  }
+});
+
+const getSelectionText = () => {
+    var text = "";
+    var activeEl = document.activeElement;
+    var activeElTagName = activeEl ? activeEl.tagName.toLowerCase() : null;
+    if (
+      (activeElTagName == "textarea") || (activeElTagName == "input" &&
+      /^(?:text|search|password|tel|url)$/i.test(activeEl.type)) &&
+      (typeof activeEl.selectionStart == "number")
+    ) {
+        text = activeEl.value.slice(activeEl.selectionStart, activeEl.selectionEnd);
+    } else if (window.getSelection) {
+        text = window.getSelection().toString();
+    }
+    selectedText = text;
+    return text;
+}
+
+const onAdd = (type) => {
+    console.log(displayedText)
+    console.log(selectedText)
+    console.log(displayedText.indexOf(selectedText) !== -1)
+    displayedText = displayedText.replace(`${selectedText}`, `<${type}>${selectedText}</${type}>`)
+}
+
 </script>
 
 <div style="text-align: center; width: 100%; margin: 0 auto;">
     <h1>Editor</h1>
     <div id="wrapper" class='wrapper'>
         <div class='texterea'>
-            <textarea bind:value={displayedText}/>
+            <ul class="menu">
+                <li on:click={() => onAdd('code')}>code</li>
+                <li on:click={() => onAdd('h1')}>h1</li>
+                <li on:click={() => onAdd('h2')}>h2</li>
+                <li on:click={() => onAdd('h3')}>h3</li>
+            </ul>
+            <textarea on:select={getSelectionText} bind:value={displayedText}/>
                 </div>
                 </div>
-                <button on:click={onSave}>click</button>
+                <!-- <button on:click={onSave}>click</button> -->
                 </div>
 
 <style>
@@ -59,7 +95,21 @@ const onSave = () => {
     width: 600px;
     border: 1px solid black;
 }
-
+.code {
+    background: #afafaf61;
+    padding: 5px 10px;
+}
+.menu {
+    display: flex;
+    justify-content: space-around;
+}
+.menu li:hover {
+    background-color: rgba(214, 214, 214, 0.733);
+}
+.menu li {
+    cursor: pointer;
+    padding: 5px 10px;
+}
 .wrapper {
     display: flex;
     justify-content: space-between;
